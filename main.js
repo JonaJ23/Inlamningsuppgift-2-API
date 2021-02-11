ce1ecb1a0fcb5a0a0cd1cff385a9a421000831c4
Jonatan Jonsson <jonatan.jonsson@utb.ecutbildning.se>
2021-02-11 10:33:56 +0100

    Enabled API to search for cities.

--- main.js
+++ main.js
@@ -1,21 +1,95 @@
-const API_key = "1c39d2bbe79193bd9cf7413fc49c0d29"
+const api = {
+    key: "1c39d2bbe79193bd9cf7413fc49c0d29",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
 
-window.onload = function() {
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
 
-
-    var checkboxes = document.getElementById("checksId");
-    var checks = checkboxes.getElementsByTagName("input");
-    for (var i = 0; i < checks.length; i++) {
-        checks[i].onclick = function() {
-            for (var i = 0; i < checks.length; i++) {
-                if (checks[i] != this && this.checked) {
-                    checks[i].checked = false;
-                }
-            }
-        };
+function setQuery(event) {
+    if (event.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
     }
-};
+}
+
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();      
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+    debugger;
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+    "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    
+    return `${day} ${date} ${month} ${year}`;
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function searchFilter() {
+    let searchBox = document.getElementById('search-box');
+    let searchString = searchBox.value;
+
+    console.log(searchString);
+
+    let Wcheck = document.getElementById('weatherCheck');
+    let Acheck = document.getElementById('attractionsCheck');
+
+    let weather = document.querySelector('#location');
+    let attraction = document.querySelector('#attraction');
+
+    if (Wcheck.checked === true) {
+        weather.style.display = 'flex';
+        attraction.style.display ='none';
+    } 
+    else (Wcheck.checked === false) {
+        weather.style.display = 'flex';
+    }
+        weather.style.display = 'none';
+    }
+    if (Acheck.checked === true) {
+        attraction.style.display = 'flex';
+    } 
+    else {
+        attraction.style.display = 'none';
+    } */
